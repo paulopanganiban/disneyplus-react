2c7f2ecbabf426b18727de8a9544cd72361fbb90
John Paulo Panganiban <olopangs@gmail.com>
2021-04-24 19:34:55 +0800

    added npm react router dom and styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
-
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-    <h1>hello</h1>
+      <Router>
+
+      </Router>
     </div>
   );
 }
